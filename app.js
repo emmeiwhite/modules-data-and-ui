0cf48150fe1fc5eea7cb6a889d1481abe60f4ab7
emmeiwhite <emmeiwhite@gmail.com>
2024-10-17 10:40:59 +0530

    Array of objects rendered imperatively in DOM

---
+++ app.js
@@ -0,0 +1,31 @@
+const persons = [
+  {
+    id: 1,
+    name: 'John Doe',
+    job: 'Software Engineer'
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    job: 'Data Scientist'
+  },
+  {
+    id: 3,
+    name: 'Emily Johnson',
+    job: 'Product Manager'
+  }
+]
+
+const personsDOM = document.querySelector('.persons-wrapper')
+const btnDOM = document.querySelector('.btn')
+
+btnDOM.addEventListener('click', () => {
+  const personsUIString = persons
+    .map(person => {
+      const { name, job } = person
+      return `<p><strong>Name</strong>: ${name} & <strong>Profession</strong>: ${job}</p>`
+    })
+    .join('')
+
+  personsDOM.innerHTML = personsUIString
+})
